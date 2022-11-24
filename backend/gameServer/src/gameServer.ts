4b0413b77079ded779ce441e556246d1aba0b38f
Bence Papp <bencep4pp@gmail.com>
2022-11-24 23:56:02 +0100

    Auth WIP

--- backend/gameServer/src/gameServer.ts
+++ backend/gameServer/src/gameServer.ts
@@ -1,3 +1,4 @@
+import { ServerWebSocket } from "bun";
 import { gamePort } from "../../../config";
 import { Game } from "./game";
 import { MoveRequest } from "./helpers/types";
@@ -9,20 +10,14 @@ export function initGameServer(game: Game) {
     port: port,
     websocket: {
       message(ws, message) {
+        // TODO check if correct color moves
         if (typeof message !== "string") return;
         const data = JSON.parse(message);
-        // TODO token validation
-        if (data.playerToken !== game.getNextPlayerToken()) {
-          // console.log(`Token received: ${data.playerToken}`);
-          // console.log(`Expected token: ${game.getNextPlayerToken()}`);
-          // return;
-          console.log("Token mismatch, but continuing anyway");
-        }
         game.tryToMove(data as MoveRequest);
         ws.publish("sendState", JSON.stringify(game.fetchGameState()));
       },
-      open(ws) {
-        console.log('Websocket opened');
+      open(ws: ServerWebSocket) {
+        game.joinPlayer(ws);
         ws.subscribe("sendState");
         ws.publish("sendState", JSON.stringify(game.fetchGameState()));
       },
@@ -32,10 +27,10 @@ export function initGameServer(game: Game) {
       // Upgrade to a ServerWebSocket if we can
       // This automatically checks for the Sec-WebSocket-Key header
       // meaning you don't have to check headers, you can just call upgrade()
-      if (server.upgrade(req))
+      if (server.upgrade(req)) {
         // When upgrading, we return undefined since we don't want to send a Response
         return;
-
+      }
       return new Response('Regular HTTP response');
     },
   });
