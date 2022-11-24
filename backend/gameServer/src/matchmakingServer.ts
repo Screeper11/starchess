07272b2d6f372aba6e9bc191a4b1f85eecf39ce6
Bence Papp <bencep4pp@gmail.com>
2022-11-24 11:18:38 +0100

    Auth

---
+++ backend/gameServer/src/matchmakingServer.ts
@@ -0,0 +1,34 @@
+import { matchmakingPort } from "../../../config";
+
+const port = matchmakingPort;
+
+export function initMatchmakingServer() {
+  const server = Bun.serve({
+    port: port,
+    websocket: {
+      message(ws, message) {
+        ws.send(message);
+      },
+      open(ws) {
+        // ws.subscribe("whiteMoves");
+        // ws.subscribe("blackMoves");
+        // ws.subscribe("sendMoveToBoth");
+        // ws.publish("sendMoveToBoth", "testData")
+        // console.log('SERVER: Websocket opened');
+      },
+      fetch(req, server) {
+        // Upgrade to a ServerWebSocket if we can
+        // This automatically checks for the Sec-WebSocket-Key header
+        // meaning you don't have to check headers, you can just call upgrade()
+        if (server.upgrade(req))
+          // When upgrading, we return undefined since we don't want to send a Response
+          return;
+
+        return new Response('Regular HTTP response');
+      },
+    }
+  });
+  console.log(`Game server is listening on port ${port}`);
+
+  return server;
+}
