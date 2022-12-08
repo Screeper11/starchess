bcd29cb394ff08b26115d9920d22177a9527167c
Bence Papp <bencep4pp@gmail.com>
2022-12-08 03:09:35 +0100

    Tests

--- backend/gameServer/test/server.test.ts
+++ backend/gameServer/test/server.test.ts
@@ -2,8 +2,46 @@ import { describe, expect, it } from 'bun:test'
 
 describe('Server is live', () => {
   it('Should return 200 Response', async () => {
-    const req = new Request('https://localhost:8080/');
-    const res = await app.fetch(req);
+    const res = await fetch('http://localhost:8080');
+    expect(res.status).toBe(200);
+  })
+});
+
+describe('Register', () => {
+  it('Should return 200 Response', async () => {
+    const res = await fetch('http://localhost:8080/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'testUser',
+        password: 'testPassword',
+      }),
+    });
+    expect(res.status).toBe(200);
+  })
+});
+
+describe('Login', () => {
+  it('Should return 200 Response', async () => {
+    const res = await fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'testUser',
+        password: 'testPassword',
+      }),
+    });
+    expect(res.status).toBe(200);
+  })
+});
+
+describe('User exists', () => {
+  it('Should return 200 Response', async () => {
+    const res = await fetch('http://localhost:8080/userExists/testUser');
     expect(res.status).toBe(200);
   })
 });
