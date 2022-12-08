3fd5a39bb8f75979b3ae364f06cf35b1b215713a
Bence Papp <bencep4pp@gmail.com>
2022-12-08 03:28:45 +0100

    mkdir

--- backend/gameServer/src/main.ts
+++ backend/gameServer/src/main.ts
@@ -1,7 +1,12 @@
 import { initServer } from "./server";
 import { SqliteDb } from "./db";
 import { Matchmaker } from "./matchmaker";
+import { mkdir } from "fs";
 
-const db = new SqliteDb("./src/database/auth.sqlite");
+const dbFolderPath = "./src/database/";
+const dbName = "auth.sqlite";
+
+mkdir(dbFolderPath, { recursive: true }, error => { if (error) throw error; });
+const db = new SqliteDb(dbFolderPath + dbName);
 const matchmaker = new Matchmaker();
 const server = initServer(db, matchmaker);
