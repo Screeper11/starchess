0c45ccd56075e2c2998c71380c758d673db135d5
Bence Papp <bencep4pp@gmail.com>
2023-01-20 14:37:33 +0100

    URL fix

--- backend/gameServer/env.ts
+++ backend/gameServer/env.ts
@@ -1,3 +1,3 @@
-export const FRONTEND_URL = "backend.starchess.screeper.dev";
+export const FRONTEND_URL = "starchess.screeper.dev";
 export const BACKEND_PORT = 8080;
 export const VERSION_NUMBER = "1.0.0";
